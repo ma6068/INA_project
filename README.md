9afec64020b21f569319a5a9c3b6e83cdd51c487
ma6068 <ma6068@student.uni-lj.si>
2022-04-18 18:35:07 +0200

    novi podatoci

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # INA_project
+
+#### Csv data (index. meaning)
+```Language
+0. league,
+1. season,
+2. window,
+3. team_id,
+4. team_name,
+5. team_country,
+6. dir,
+7. player_id,
+8. player_name,
+9. player_age,
+10. player_nation,
+11. player_nation2,
+12. player_pos,
+13. counter_team_id,
+14. counter_team_name,
+15. counter_team_country,
+16. transfer_fee_amnt,
+17. market_val_amnt,
+18. is_free,
+19. is_loan,
+20. is_loan_end,
+21. is_retired,
+22. transfer_id
+```
--- create_data.py
+++ create_data.py
@@ -1,96 +1,44 @@
-english_teams = ['Arsenal FC', 'Chelsea FC', 'Newcastle United', 'Fulham FC', 'West Bromwich Albion',
-                 'Manchester United', 'Queens Park Rangers', 'Wigan Athletic', 'Norwich City',
-                 'Wolverhampton Wanderers', 'Stoke City', 'Everton FC', 'Liverpool FC', 'Tottenham Hotspur',
-                 'Blackburn Rovers', 'Manchester City', 'Sunderland AFC', 'Bolton Wanderers', 'Aston Villa',
-                 'Swansea City', 'West Ham United', 'Southampton FC', 'Reading FC', 'Cardiff City', 'Hull City',
-                 'Crystal Palace', 'Burnley FC', 'Leicester City', 'AFC Bournemouth', 'Watford FC', 'Middlesbrough FC',
-                 'Brighton & Hove Albion', 'Huddersfield Town', 'Sheffield United', 'Leeds United', 'Brentford FC']
+import os
 
-french_teams = ['Olympique Lyon', 'FC Évian Thonon Gaillard', 'Stade Brestois 29', 'Dijon FCO', 'Valenciennes FC',
-                'SM Caen', 'AS Nancy-Lorraine', 'FC Lorient', 'AC Ajaccio', 'LOSC Lille', 'FC Girondins Bordeaux',
-                'Montpellier HSC', 'FC Sochaux-Montbéliard', 'AS Saint-Étienne', 'Paris Saint-Germain', 'OGC Nice',
-                'FC Toulouse', 'AJ Auxerre', 'Stade Rennais FC', 'Olympique Marseille', 'Stade Reims', 'ESTAC Troyes',
-                'SC Bastia', 'AS Monaco', 'EA Guingamp', 'FC Nantes', 'RC Lens', 'FC Metz', 'SCO Angers', 'GFC Ajaccio',
-                'Amiens SC', 'RC Strasbourg Alsace', 'Nîmes Olympique', 'Clermont Foot 63']
-
-german_teams = ['Bayern Munich', 'Borussia Dortmund', 'Bayer 04 Leverkusen', 'FC Schalke 04', 'SC Freiburg',
-                'Hamburger SV', 'Borussia Mönchengladbach', 'Hannover 96', '1.FC Nuremberg', 'VfL Wolfsburg',
-                'VfB Stuttgart', '1.FSV Mainz 05', 'SV Werder Bremen', 'FC Augsburg', 'TSG 1899 Hoffenheim',
-                'Hertha BSC', '1. FC Köln', '1.FC Kaiserslautern', 'Eintracht Frankfurt', 'SpVgg Greuther Fürth',
-                'Fortuna Düsseldorf', 'Eintracht Braunschweig', 'SC Paderborn 07', 'FC Ingolstadt 04',
-                'SV Darmstadt 98', 'RB Leipzig', '1.FC Union Berlin', 'Arminia Bielefeld', 'VfL Bochum']
-
-italian_teams = ['Atalanta BC', 'Novara Calcio 1908', 'SSC Napoli', 'Calcio Catania', 'AC Cesena', 'Cagliari Calcio',
-                 'AC Siena', 'Bologna FC 1909', 'US Lecce', 'Chievo Verona', 'AC Milan', 'Juventus FC', 'US Palermo',
-                 'ACF Fiorentina', 'Udinese Calcio', 'SS Lazio', 'Genoa CFC', 'Parma FC', 'FC Internazionale',
-                 'AS Roma', 'Delfino Pescara 1936', 'Torino FC', 'UC Sampdoria', 'US Sassuolo', 'AS Livorno',
-                 'Hellas Verona', 'Empoli FC', 'Inter Milan', 'Carpi FC 1909', 'Frosinone Calcio', 'FC Crotone',
-                 'Benevento Calcio', 'SPAL 2013', 'Parma Calcio 1913', 'SPAL', 'Brescia Calcio', 'Spezia Calcio',
-                 'FC Empoli', 'US Salernitana 1919', 'Venezia FC']
-
-spanish_teams = ['Racing Santander', 'Granada CF', 'Getafe CF', 'Levante UD', 'Sporting Gijón', 'Málaga CF',
-                 'Villarreal CF', 'Valencia CF', 'RCD Espanyol Barcelona', 'Real Sociedad', 'Atlético de Madrid',
-                 'Real Madrid', 'Sevilla FC', 'Rayo Vallecano', 'CA Osasuna', 'RCD Mallorca', 'Real Betis Balompié',
-                 'Real Zaragoza', 'FC Barcelona', 'Athletic Bilbao', 'Celta de Vigo', 'Deportivo de La Coruña',
-                 'Real Valladolid CF', 'UD Almería', 'Elche CF', 'SD Eibar', 'Córdoba CF', 'UD Las Palmas',
-                 'Deportivo Alavés', 'CD Leganés', 'Girona FC', 'SD Huesca', 'Cádiz CF']
-
-
-all_teams = ['Arsenal FC', 'Chelsea FC', 'Newcastle United', 'Fulham FC', 'West Bromwich Albion', 'Manchester United',
-             'Queens Park Rangers', 'Wigan Athletic', 'Norwich City', 'Wolverhampton Wanderers', 'Stoke City',
-             'Everton FC', 'Liverpool FC', 'Tottenham Hotspur', 'Blackburn Rovers', 'Manchester City', 'Sunderland AFC',
-             'Bolton Wanderers', 'Aston Villa', 'Swansea City', 'West Ham United', 'Southampton FC', 'Reading FC',
-             'Cardiff City', 'Hull City', 'Crystal Palace', 'Burnley FC', 'Leicester City', 'AFC Bournemouth',
-             'Watford FC', 'Middlesbrough FC', 'Brighton & Hove Albion', 'Huddersfield Town', 'Sheffield United',
-             'Leeds United', 'Brentford FC', 'Olympique Lyon', 'FC Évian Thonon Gaillard', 'Stade Brestois 29',
-             'Dijon FCO', 'Valenciennes FC', 'SM Caen', 'AS Nancy-Lorraine', 'FC Lorient', 'AC Ajaccio', 'LOSC Lille',
-             'FC Girondins Bordeaux', 'Montpellier HSC', 'FC Sochaux-Montbéliard', 'AS Saint-Étienne',
-             'Paris Saint-Germain', 'OGC Nice', 'FC Toulouse', 'AJ Auxerre', 'Stade Rennais FC', 'Olympique Marseille',
-             'Stade Reims', 'ESTAC Troyes', 'SC Bastia', 'AS Monaco', 'EA Guingamp', 'FC Nantes', 'RC Lens', 'FC Metz',
-             'SCO Angers', 'GFC Ajaccio', 'Amiens SC', 'RC Strasbourg Alsace', 'Nîmes Olympique', 'Clermont Foot 63',
-             'Bayern Munich', 'Borussia Dortmund', 'Bayer 04 Leverkusen', 'FC Schalke 04', 'SC Freiburg',
-             'Hamburger SV', 'Borussia Mönchengladbach', 'Hannover 96', '1.FC Nuremberg', 'VfL Wolfsburg',
-             'VfB Stuttgart', '1.FSV Mainz 05', 'SV Werder Bremen', 'FC Augsburg', 'TSG 1899 Hoffenheim',
-             'Hertha BSC', '1. FC Köln', '1.FC Kaiserslautern', 'Eintracht Frankfurt', 'SpVgg Greuther Fürth',
-             'Fortuna Düsseldorf', 'Eintracht Braunschweig', 'SC Paderborn 07', 'FC Ingolstadt 04', 'SV Darmstadt 98',
-             'RB Leipzig', '1.FC Union Berlin', 'Arminia Bielefeld', 'VfL Bochum', 'Atalanta BC', 'Novara Calcio 1908',
-             'SSC Napoli', 'Calcio Catania', 'AC Cesena', 'Cagliari Calcio', 'AC Siena', 'Bologna FC 1909', 'US Lecce',
-             'Chievo Verona', 'AC Milan', 'Juventus FC', 'US Palermo', 'ACF Fiorentina', 'Udinese Calcio', 'SS Lazio',
-             'Genoa CFC', 'Parma FC', 'FC Internazionale', 'AS Roma', 'Delfino Pescara 1936', 'Torino FC',
-             'UC Sampdoria', 'US Sassuolo', 'AS Livorno', 'Hellas Verona', 'Empoli FC', 'Inter Milan', 'Carpi FC 1909',
-             'Frosinone Calcio', 'FC Crotone', 'Benevento Calcio', 'SPAL 2013', 'Parma Calcio 1913', 'SPAL',
-             'Brescia Calcio', 'Spezia Calcio', 'FC Empoli', 'US Salernitana 1919', 'Venezia FC', 'Racing Santander',
-             'Granada CF', 'Getafe CF', 'Levante UD', 'Sporting Gijón', 'Málaga CF', 'Villarreal CF', 'Valencia CF',
-             'RCD Espanyol Barcelona', 'Real Sociedad', 'Atlético de Madrid', 'Real Madrid', 'Sevilla FC',
-             'Rayo Vallecano', 'CA Osasuna', 'RCD Mallorca', 'Real Betis Balompié', 'Real Zaragoza', 'FC Barcelona',
-             'Athletic Bilbao', 'Celta de Vigo', 'Deportivo de La Coruña', 'Real Valladolid CF', 'UD Almería',
-             'Elche CF', 'SD Eibar', 'Córdoba CF', 'UD Las Palmas', 'Deportivo Alavés', 'CD Leganés', 'Girona FC',
-             'SD Huesca', 'Cádiz CF']
-
-
-# import os
-# teams = []
-# for filename in os.listdir('./data'):
-#     with open(os.path.join('./data', filename), 'r', encoding='utf8') as f:
-#         f.readline()
-#         alldata = f.readlines()
-#         for el in alldata:
-#             club = el.split(',')
-#             if club[0] not in teams:
-#                 teams.append(club[0])
-
-f = open("Teams.txt", "w", encoding='utf8')
-f.write('Team, Club_id, Country \n')
-for i in range(len(all_teams)):
-    league = -1
-    if all_teams[i] in english_teams:
-        league = 'England'
-    elif all_teams[i] in french_teams:
-        league = 'France'
-    elif all_teams[i] in german_teams:
-        league = 'Germany'
-    elif all_teams[i] in italian_teams:
-        league = 'Italy'
-    else:
-        league = 'Spain'
-    f.write(all_teams[i] + ',' + str(i) + ',' + league + '\n')
+f2 = open('data.txt', 'w', encoding='utf8')
+f2.write('Team1_Id, Team1_name, Country1, Direction, Position, Team2_id, Team2_name, Country2, Price \n')
+for filename in os.listdir('./data'):
+    with open(os.path.join('./data', filename), 'r', encoding='utf8') as f:
+        f.readline()  # read first row
+        alldata = f.readlines()
+        for el in alldata:
+            data = el.split(',')
+            if int(data[1]) >= 2011 and (data[0] == 'GB1' or data[0] == 'ES1' or data[0] == 'IT1' or data[0] == 'L1' or data[0] == 'FR1'):
+                team1_id = data[3]
+                team1_name = data[4]
+                team1_country = data[5]
+                direction = data[6]
+                position = data[13]
+                team2_id = data[14]
+                team2_name = data[15]
+                team2_country = data[15]
+                price1 = data[16]
+                price2 = data[17]
+                # nekade ima loso ime na drzava (pr. Korea, South)
+                if len(data) == 24:
+                    position = data[13]
+                    team2_id = data[14]
+                    team2_name = data[15]
+                    team2_country = data[16]
+                    price1 = data[17]
+                    price2 = data[18]
+                if len(data) == 25:
+                    position = data[13]
+                    team2_id = data[14]
+                    team2_name = data[15]
+                    team2_country = data[16]
+                    price1 = data[18]
+                    price2 = data[19]
+                if not price1:
+                    price1 = 0
+                if not price2:
+                    price2 = 0
+                price = float(price1) + float(price2)
+                if team2_country != 'England' and team2_country != 'France' and team2_country != 'Germany' and team2_country != 'Italy' and team2_country != 'Spain':
+                    team2_country = 'Other'
+                f2.write(team1_id + ',' + team1_name + ',' + team1_country + ',' + direction + ',' + position + ',' + team2_id + ',' + team2_name + ',' + team2_country + ',' + str(price) + '\n')
