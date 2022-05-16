d04bc5950de963cd912d57110a560f0f641c0509
ma6068 <ma6068@student.uni-lj.si>
2022-05-16 11:07:35 +0200

    trofei

---
+++ trophies.py
@@ -0,0 +1,13 @@
+best_teams_ids = [985, 281, 31, 631, 11,
+                  583, 244, 162, 1041, 1082,
+                  27, 16, 23826, 18, 33,
+                  5, 46, 506, 12, 398,
+                  418, 131, 13, 1049, 621]
+
+best_teams = ['Manchester United', 'Manchester City', 'Liverpool FC', 'Chelsea FC', 'Arsenal FC',
+              'Paris Saint-Germain', 'Olympique Marseille', 'AS Monaco', 'Olympique Lyon', 'LOSC Lille',
+              'Bayern Munich', 'Borussia Dortmund', 'RB Leipzig', 'Borussia Mönchengladbach', 'FC Schalke 04',
+              'AC Milan', 'Inter Milan', 'Juventus FC', 'AS Roma', 'SS Lazio',
+              'Real Madrid', 'FC Barcelona', 'Atlético de Madrid', 'Valencia CF', 'Athletic Bilbao']
+
+trofei = []
